6b672af71f6938d899c1e51a85f84f7aef7a758b
pchr-srf <philipp.christen@srf.ch>
2019-03-23 11:20:33 +0100

    unsuccessful try with a larger surface

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import { ReactInstance, Surface } from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -10,11 +10,23 @@ function init(bundle, parent, options = {}) {
     ...options,
   });
 
+  const mainSurfaceWidth = 4096;
+  const mainSurfaceHeight = 700;
+  const mainSurface = new Surface(mainSurfaceWidth, mainSurfaceHeight);
+  mainSurface.setDensity(mainSurfaceWidth);
+
   // Render your app content to the default cylinder surface
   r360.renderToSurface(
-    r360.createRoot('Hello360', { /* initial props */ }),
-    r360.getDefaultSurface()
+    r360.createRoot('Hello360', {
+      useDynamicSurface: false,
+      mainSurfaceWidth: mainSurfaceWidth,
+      mainSurfaceHeight: mainSurfaceHeight
+    }),
+    mainSurface
+    //r360.getDefaultSurface()
   );
+
+
 
   // Load the initial environment
   r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
--- index.js
+++ index.js
@@ -1,49 +1,56 @@
 import React from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  View,
+    AppRegistry,
+    StyleSheet,
+    View,
 } from 'react-360';
 import { Landingpage } from './src/components/Landingpage';
 import { Article } from './src/components/Article';
 
 export default class Hello360 extends React.Component {
-  state = {
-      articleToDisplay: null
-  };
+    state = {
+        articleToDisplay: null
+    };
 
-  setArticle = (articleId) =>  {
-      console.log(articleId);
-      this.setState({
-          articleToDisplay: articleId
-      })
-  };
+    setArticle = (articleId) =>  {
+        console.log(articleId);
+        this.setState({
+            articleToDisplay: articleId
+        })
+    };
 
-  render() {
-    if (this.state.articleToDisplay) {
+    render() {
         return (
-            <View style={styles.panel}>
-                <Article urn={this.state.articleToDisplay}/>
-            </View>
-        )
-    } else {
-        return (
-            <View style={styles.panel}>
-                <Landingpage setArticle={this.setArticle}/>
+            <View style={styles.panelContainer}>
+
+                <View style={styles.panel}>
+                    <Landingpage setArticle={this.setArticle}/>
+                </View>
+
+                { this.state.articleToDisplay && (
+                    <View style={styles.panel}>
+                        <Article urn={this.state.articleToDisplay}/>
+                    </View>
+                )}
             </View>
         );
-
     }
-  }
 };
 
 const styles = StyleSheet.create({
-  panel: {
-    width: 1000,
-    height: 600,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  }
+    panelContainer: {
+        width: '100%',
+        height: '100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    panel: {
+        width: 1000,
+        height: 600,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    }
 });
 
 AppRegistry.registerComponent('Hello360', () => Hello360);
