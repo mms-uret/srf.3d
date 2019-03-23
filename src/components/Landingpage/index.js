e5ed11e19398fefca1d215917fe410be14594f2d
pchr-srf <philipp.christen@srf.ch>
2019-03-23 11:24:19 +0100

    doubled gemobbelt

--- src/components/Landingpage/index.js
+++ src/components/Landingpage/index.js
@@ -80,7 +80,6 @@ const styles = StyleSheet.create({
     },
     landingPageWrapper: {
         flexDirection: 'row',
-        alignItems: 'stretch',
         alignItems: 'center',
         justifyContent: 'center'
     },
